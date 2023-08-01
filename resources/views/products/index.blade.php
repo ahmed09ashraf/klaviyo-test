6cb22c90d46b0f36067caaee47c537ded45b8a00
ahmed09ashraf <ahmedashraf097@gmail.com>
2023-08-01 13:00:42 +0300

    search modify

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -28,7 +28,7 @@
             background-color: #dddddd;
             color: grey;
             padding: 10px;
-            width:100% ;
+            width: 70px;
             font-size: 16px;
             border: none;
             cursor:pointer ;
@@ -47,17 +47,18 @@
             font-size: 16px;
             padding: 14px 20px 12px 45px;
             border: none;
-            border-bottom: 1px solid #ddd;
-            width:1200px;
+            border-bottom: 1px solid #ffb243;
+            width:95%;
         }
 
         /* The search field when it gets focus/clicked on */
-        #myInput:focus {outline: 3px solid #ddd;}
+        #myInput:focus {outline: 3px solid #ffb243;}
 
         /* The container <div> - needed to position the dropdown content */
         .dropdown {
             position: relative;
             display: inline-block;
+            width: 100%;
         }
 
         /* Dropdown Content (Hidden by Default) */
@@ -66,6 +67,7 @@
             position: absolute;
             background-color: #f9f9f9;
             height:100px;
+            width: 100%;
             border: 1px solid #ddd;
             z-index: 999;
             transform: translateY(-10px);
@@ -82,16 +84,16 @@
 
         /* Show the dropdown menu (use JS to add this class to the .dropdown-content container when the user clicks on the dropdown button) */
         .show {
-            display: block;
             opacity: 1;
             visibility: visible;
             height:620px ;
+            /* width: 100%; */
             transform: translateY(0);
             transition-delay: 0s, 0s, 0s;
         ;}
 
         ul{
-        border-right: #ddd 2px solid;
+        border-right: #ffb243 2px solid;
         width: 100%;
         margin-right: 80px;
         }
@@ -104,7 +106,7 @@
         }
 
         li:hover{
-            background-color: #718096;
+            background-color: #ffb243;
         }
 
         .arrow{
@@ -117,33 +119,29 @@
         }
 
         .arrow-left :hover , .arrow-right :hover{
-            color :grey ;
+            color :#ffb243 ;
         }
 
-
-        .nav{
-        display :none;
-            transform: translateY(-10px);
-            /* opacity: 0;
-            visibility: hidden; */
-            top: 100%;
-            left: 0;
-            box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
-            
-            box-sizing: border-box;
-            transition: transform 0.3s ease, opacity 0.3s ease, visibility 0s linear 0.3s;
-            justify-content: space-between;
-            flex-wrap: nowrap;
-            position: absolute;
-            width:100% ;
-            height:600px ;
-            background-color: red;
-            margin :5px ;
-            padding :10px ;
-            overflow: hidden;
-            z-index: 999;
-
+        .product{
+            transition: transform 0.2s, box-shadow 0.2s;
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+            text-align: center;
+            width: 80%;
         }
+        .product:hover{
+            border-radius: 5%;
+            background-color: #ffb243;
+            cursor: pointer;
+            transform: scale(1.05);
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+        }
+
+        .product:focus{
+            outline: none;
+            transform: scale(1.05);
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+        }
+
 
     </style>
 
@@ -151,115 +149,8 @@
 <body>
 
 
-
-<div   class="dropbtn"  onclick="myFunction()">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="50" height="50" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
-                    <path d="M21 21l-6 -6"></path>
-                </svg>
-            </div>
-
-
-<div class="nav" id="myNav">
-                        <!-- Sidebar content -->
-                        <div>
-                            <ul>
-                                <h2>Catalogue</h2>
-                                <li>First</li>
-                                <li>Second</li>
-                                <li>Third</li>
-                                <li>Fourth</li>
-                                <li>Fifth</li>
-                                <li>Sixth</li>
-                                <li>Seventh</li>
-                            </ul>
-
-                        </div>
-
-                        <!-- Products content -->
-                        <div>
-
-                            <div class="arrow">
-                                    <i class="arrow-left">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-left" width="50" height="50" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                        <path d="M15 6l-6 6l6 6"></path>
-                                    </svg>
-                                    </i>
-
-                                    <i class="arrow-right">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-right" width="50" height="50" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                            <path d="M9 6l6 6l-6 6"></path>
-                                        </svg>
-                                    </i>
-                            </div>
-                            <div class="row">
-
-                                
-                                    <div class="product-card product">
-                                        <img class="img-fluid" style="min-height: 130px;max-height: 200px;" src="https://images.awesomebooks.com/images/books/medium/97805/9780571376483.jpg" alt="Demon Copperhead">
-                                        <h5>Demon Copperhead</h5>
-                                        <i>xx</i>
-                                        <b>12 $</b>
-                                    
-                                </div>
-
-                                    <div class="product-card product">
-                                        <img class="img-fluid" style="min-height: 130px;max-height: 200px;" src="https://images.awesomebooks.com/images/books/medium/97817/9781786892737.jpg" alt="The Midnight Library">
-                                        <h5>The Midnight Library</h5>
-                                        <i>xx</i>
-                                        <b>24 $</b>
-                                    
-                                </div>
-
-                                
-                                    <div class="product-card product">
-                                        <img class="img-fluid" style="min-height: 130px;max-height: 200px;" src="https://images.awesomebooks.com/images/books/medium/97808/9780861546732.jpg" alt="The Way I Used to Be">
-                                        <h5>The Way I Used to Be</h5>
-                                        <i>xx</i>
-                                        <b>15 $</b>
-                                   
-                                </div>
-
-                          
-                                    <div class="product-card product">
-                                        <img class="img-fluid" style="min-height: 130px;max-height: 200px;" src="https://images.awesomebooks.com/images/books/medium/97817/9781728205489.jpg" alt="If He Had Been with Me">
-                                        <h5>If He Had Been with Me</h5>
-                                        <i>xx</i>
-                                        <b>14 $</b>
-                                </div>
-                               
-
-                              
-                                  <!-- <div class="product-card product">
-                                        <img class="img-fluid" style="min-height: 130px;max-height: 200px;" src="https://images.awesomebooks.com/images/books/medium/97805/9780571365425.jpg" alt="Beautiful World, Where Are You">
-                                        <h5>Beautiful World, Where Are You</h5>
-                                        <i>xx</i>
-                                        <b>15 $</b>
-                                   </div>
-                               -->
-
-{{--                                <div class="col-md-4">--}}
-{{--                                    <div class="product-card product">--}}
-{{--                                        <img class="img-fluid" style="min-height: 130px;max-height: 200px;" src="https://images.awesomebooks.com/images/books/medium/97818/9781838770013.jpg" alt="The Beekeeper of Aleppo">--}}
-{{--                                        <h5>The Beekeeper of Aleppo</h5>--}}
-{{--                                        <i>xx</i>--}}
-{{--                                        <b>15 $</b>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-
 </div>
+<div style="width:100%">
         <div class="dropdown">
             <div   class="dropbtn"  onclick="myFunction()">
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="50" height="50" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -272,8 +163,8 @@
             <div id="myDropdown"  class="dropdown-content"  >
                 <input type="text" placeholder="Search.." id="myInput" onkeyup="filterFunction()">
 
-                <div class="container-fluid">
-                    <div class="row">
+                <div class="container-fluid" >
+                    <div class="row ">
 
                         <!-- Sidebar content -->
                         <div class="col-md-3">
@@ -372,7 +263,7 @@
                 </div>
             </div>
         </div>
-
+    </div>
 
         <div class="container">
             <h1>Products</h1>
@@ -493,7 +384,7 @@
 
         <script>
             function myFunction() {
-                document.getElementById("myNav").classList.toggle("show");
+                document.getElementById("myDropdown").classList.toggle("show");
             }
         </script>
 
